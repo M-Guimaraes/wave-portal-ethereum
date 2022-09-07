680a139e61b124361a2e32fa7edd9c33476f0a50
Marcelo Guimaraes da Silva <marcelo.silva@toroinvestimentos.com.br>
2022-09-07 20:44:55 -0300

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+const main = async () => {
+  const waveContractFactory = await ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: ethers.utils.parseEther("0.001"),
+  });
+
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
